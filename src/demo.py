6864e6e1d462dd9087c9ead49bbdbba15c098449
Rahul <rahul69828@gmail.com>
2026-03-27 11:20:34 +0530

    first commit

---
+++ src/demo.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a+b
+
+
+if __name__ == "__main__":
+    print(f"Adding two nos 2 and 3 gives {add(2, 3)}")
---
+++ tests/test_demo.py
@@ -0,0 +1,6 @@
+
+import pytest
+from demo import add
+
+def test_add():
+    assert add(2,3) == 5
